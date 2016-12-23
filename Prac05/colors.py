9e3bd97167d26a31d02eec490cc154f34828182b
jc451073 <josmianujose.moolayil@my.jcu.edu.au>
2016-12-23 20:55:23 +1000

    activities

--- Prac05/colors.py
+++ Prac05/colors.py
@@ -1,9 +1,14 @@
 __author__ = 'jc451073'
 Color_list={"beige": "#f5f5dc","bisque1": "#ffe4c4","bisque3": "#cdb79e","blue1": "#0000ff","blue2": "#0000ee",}
+
+
+print(Color_list['beige'])
+
 color=input("enter the color to get code")
+
 while color != " ":
     if color in Color_list:
-        print(color,"is",Color_list[color])
+        print(Color_list[color])
     else:
         print("Invalid color")
     color = input("Enter color again: ")
